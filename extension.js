4bbdf37d27f9039982dff4d6e6df61b492995ffa
Mogli <affenwiesel@gmail.com>
2018-02-22 18:11:45 +0100

    bugfixes

--- extension.js
+++ extension.js
@@ -6,31 +6,21 @@ const os = require("os");
 var channel = null;
 const fullRange = doc => doc.validateRange(new vscode.Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE));
 const MODE = { language: 'matlab', scheme: 'file' };
-class OperatingSystem {
-    constructor(name, file_ext, checker) {
-        this.name = name;
-        this.file_ext = file_ext;
-        this.checker = checker;
-    }
-}
-exports.OperatingSystem = OperatingSystem;
-const windows = new OperatingSystem('win32', '.py', 'where');
-const linux = new OperatingSystem('linux', '.py', 'which');
-const mac = new OperatingSystem('darwin', '.py', 'which');
+
 class MatlabFormatter {
     constructor() {
         this.machine_os = os.platform();
         console.log(this.machine_os);
-        this.formatter = vscode.workspace.getConfiguration('matlab-formatter')['path'];
-        if (this.machine_os == windows.name) {
-            this.formatter = 'python ' + this.formatter;
+        this.py = ''
+        if (this.machine_os == 'win32') {
+            this.py = 'python ';
         }
     }
 
     formatDocument(document) {
+        let name = document.fileName;
         return new Promise((resolve, reject) => {
-            let filename = document.fileName;
-            this.format(filename, document).then((res) => {
+            this.format(name, document).then((res) => {
                 return resolve(res);
             });
 
@@ -38,23 +28,22 @@ class MatlabFormatter {
     }
 
     format(filename, document) {
-
         return new Promise((resolve, reject) => {
-            showErrorMessage(this.formatter + ' "' + filename + '"');
-            cp.exec(this.formatter + ' "' + filename + '"', (err, stdout, stderr) => {
+            let formatter = vscode.workspace.getConfiguration('matlab-formatter')['path'];
+            cp.exec(this.py + formatter + ' "' + filename + '"', (err, stdout, stderr) => {
                 if (stdout != '') {
                     var edit = [vscode.TextEdit.replace(fullRange(document), stdout)];
                     return resolve(edit);
                 }
+                vscode.window.showErrorMessage('path to matlab_formatter not found')
                 return resolve(null);
             });
         });
     }
 }
+
 exports.MatlabFormatter = MatlabFormatter;
-function showErrorMessage(msg) {
-    vscode.window.showErrorMessage(msg);
-}
+
 class MatlabDocumentRangeFormatter {
     constructor() {
         this.formatter = new MatlabFormatter();
@@ -65,6 +54,7 @@ class MatlabDocumentRangeFormatter {
         });
     }
 }
+
 function activate(context) {
     channel = vscode.window.createOutputChannel('matlab-formatter');
     context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(MODE, new MatlabDocumentRangeFormatter()));
--- formatter/test.m
+++ formatter/test.m
@@ -12,18 +12,3 @@ for (index = 1:n)
     lsg_exact = @(y_0, t) (t .+ (1 / y_0)).^(-1);
     rechte_Seite = @(t, y) -y.^2;
 end
-
-for (index = 1:n)
-    A = [-1, -2, -5E-25 i]; b = 3
-    x = A * (-b');
-
-    x += 5; --x++
-
-    if (a <= 17 | a > 25)
-        printf('bla,blui %    blub + = 5', 55 + 5)
-    end %endif
-
-    x = 5 - a
-    lsg_exact = @(y_0, t) (t .+ (1 / y_0)).^(-1);
-    rechte_Seite = @(t, y) -y.^2;
-end
